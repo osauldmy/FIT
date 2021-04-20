ced4d94d08f119e7726f9407d8c9c6c76874530d
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-20 23:44:38 +0200

    Refactoring: move api/app.py to app.py

--- apixy/app.py
+++ apixy/app.py
@@ -1,6 +1,11 @@
 """The app's entrypoint"""
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
+from apixy.api.v1.app import app as v1_app
+from apixy.config import TORTOISE_CONFIG, settings
 
-from .api.app import app
+app = FastAPI(title=settings.APP_NAME)
+app.mount(settings.API_PREFIX + "/v1", v1_app)
 
-__all__ = ["app"]
+register_tortoise(app, config=TORTOISE_CONFIG)
