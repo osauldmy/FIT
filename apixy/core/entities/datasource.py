6c82c5d328a42203a4035de93ccc040a2cdf40e6
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-21 01:25:57 +0200

    Uncomment methods of datasources; Fix linter warnings

--- apixy/core/entities/datasource.py
+++ apixy/core/entities/datasource.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
 
-# from abc import abstractmethod
-from typing import Any, Dict, Literal, Optional
+from abc import abstractmethod
+from typing import Any, Dict, Literal, Optional, cast
 
-# import aiohttp
-# import databases
+import aiohttp
+import databases
 import jmespath
-
-# from motor import motor_asyncio
-from pydantic import AnyUrl, BaseConfig, BaseModel, HttpUrl
+import motor
+from pydantic import AnyUrl, BaseConfig, HttpUrl, validator
 
 from apixy.shared.entities import ForbidExtraModel
 
 
 # todo: will we need this anywhere else? if so, move it to shared
-class JSONPathSerializable(BaseModel):
+class JSONPathSerializable(ForbidExtraModel):
     """Adds a jmespath json encoder to the model."""
 
     class Config(BaseConfig):
@@ -23,7 +22,7 @@ class JSONPathSerializable(BaseModel):
         json_encoders = {jmespath.parser.ParsedResult: lambda x: x.expression}
 
 
-class DataSource(ForbidExtraModel, JSONPathSerializable):
+class DataSource(JSONPathSerializable):
     """An interface for fetching data from a remote source"""
 
     url: AnyUrl
@@ -31,20 +30,18 @@ class DataSource(ForbidExtraModel, JSONPathSerializable):
     # transformation/aggregation TODO
     timeout: Optional[float] = None  # TODO
 
-    class Config(ForbidExtraModel.Config, JSONPathSerializable.Config):
-        """pydantic config class"""
+    @classmethod
+    @validator("jsonpath")
+    def validate_json_path(cls, value: str) -> jmespath.parser.ParsedResult:
+        """Validator for jmespath strings"""
+        try:
+            return jmespath.compile(value)
+        except jmespath.exceptions.ParseError as exception:
+            raise ValueError("Invalid JsonPath") from exception
 
-    # @validator("jsonpath")
-    # def validate_json_path(cls: DataSource, v: str) -> jmespath.parser.ParsedResult:
-    #     """Validator for jmespath strings"""
-    #     try:
-    #         return jmespath.compile(v)
-    #     except jmespath.exceptions.ParseError:
-    #         raise ValueError("Invalid JsonPath")
-
-    # @abstractmethod
-    # async def fetch_data(self) -> Dict[str, Any]:
-    #     """Fetches data as defined by the instance's attributes"""
+    @abstractmethod
+    async def fetch_data(self) -> Dict[str, Any]:
+        """Fetches data as defined by the instance's attributes"""
 
 
 class HTTPDataSource(DataSource):
@@ -56,27 +53,21 @@ class HTTPDataSource(DataSource):
     headers: Dict[str, Any] = {}
     # TODO: credentials/token # or leave in headers?
 
-    # async def fetch_data(self) -> Dict[str, Any]:
-    #     try:
-    #         async with aiohttp.ClientSession(
-    #             timeout=aiohttp.ClientTimeout(total=self.timeout)
-    #         ) as session:
-    #             async with session.request(
-    #                 method=self.method,
-    #                 url=self.url,
-    #                 json=self.body,
-    #                 headers=self.headers,
-    #             ) as response:
-    #                 data = await response.json()
-    #
-    #         return {
-    #             "result": cast(
-    #                 jmespath.parser.ParsedResult, self.jsonpath
-    #             ).search(data)
-    #         }
-    #     except Exception as e:
-    #         print(e)
-    #         return {"result": f"fail:  {e}"}
+    async def fetch_data(self) -> Dict[str, Any]:
+        async with aiohttp.ClientSession(
+            timeout=aiohttp.ClientTimeout(total=self.timeout)
+        ) as session:
+            async with session.request(
+                method=self.method,
+                url=self.url,
+                json=self.body,
+                headers=self.headers,
+            ) as response:
+                data = await response.json()
+
+        return {
+            "result": cast(jmespath.parser.ParsedResult, self.jsonpath).search(data)
+        }
 
 
 class MongoDBDataSource(DataSource):
@@ -86,21 +77,15 @@ class MongoDBDataSource(DataSource):
     collection: str
     query: Dict[str, Any]
 
-    # async def fetch_data(self) -> Dict[str, Any]:
-    #
-    #     client = motor_asyncio.AsyncIOMotorClient(self.url)
-    #     cursor = client[self.database][self.collection].find(self.query)
-    #     data = await cursor.to_list()
-    #
-    #     try:
-    #         return {
-    #             "result": cast(
-    #                 jmespath.parser.ParsedResult, self.jsonpath
-    #             ).search(data)
-    #         }
-    #     except Exception as e:
-    #         print(e)
-    #         return {"result": f"fail:  {e}"}
+    async def fetch_data(self) -> Dict[str, Any]:
+
+        client = motor.motor_asyncio.AsyncIOMotorClient(self.url)
+        cursor = client[self.database][self.collection].find(self.query)
+        data = await cursor.to_list()
+
+        return {
+            "result": cast(jmespath.parser.ParsedResult, self.jsonpath).search(data)
+        }
 
 
 class SQLDataSource(DataSource):
@@ -110,16 +95,12 @@ class SQLDataSource(DataSource):
 
     # TODO: sql validator (allow only select)
 
-    # async def fetch_data(self) -> Dict[str, Any]:
-    #     try:
-    #         async with databases.Database(self.url) as database:
-    #             rows = await database.fetch_all(query=self.query)
-    #
-    #         return {
-    #             "result": cast(jmespath.parser.ParsedResult, self.jsonpath).search(
-    #                 [dict(row) for row in rows]
-    #             )
-    #         }
-    #     except Exception as e:
-    #         print(e)
-    #         return {"result": f"fail:  {e}"}
+    async def fetch_data(self) -> Dict[str, Any]:
+        async with databases.Database(self.url) as database:
+            rows = await database.fetch_all(query=self.query)
+
+        return {
+            "result": cast(jmespath.parser.ParsedResult, self.jsonpath).search(
+                [dict(row) for row in rows]
+            )
+        }
