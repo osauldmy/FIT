e9da9cbac1e4924231ae9cd8909591a4cc54a126
Matyas Richter <mail@mrichter.cz>
2021-04-21 21:42:23 +0200

    Made endpoint methods async.

--- apixy/api/v1/projects.py
+++ apixy/api/v1/projects.py
@@ -17,15 +17,15 @@ class ProjectCBV:
     """Provides CRUD for Projects"""
 
     @router.get("/{project_id}")
-    def get(self, project_id: UUID4) -> Project:
+    async def get(self, project_id: UUID4) -> Project:
         """Endpoint for a single project."""
 
     @router.get("/")
-    def get_list(self) -> List[Project]:
+    async def get_list(self) -> List[Project]:
         """Endpoint for GET"""
 
     @router.post("/", status_code=status.HTTP_201_CREATED, response_class=Response)
-    def create(self, project: Project) -> Response:
+    async def create(self, project: Project) -> Response:
         """Creating a new Project"""
         # create project here, on success, return like
         # return Response(
@@ -35,11 +35,11 @@ class ProjectCBV:
     @router.put(
         "/{project_id}", status_code=status.HTTP_204_NO_CONTENT, response_class=Response
     )
-    def update(self, project: Project) -> Response:
+    async def update(self, project: Project) -> Response:
         """Updating an existing Project"""
 
     @router.delete(
         "/{project_id}", status_code=status.HTTP_204_NO_CONTENT, response_class=Response
     )
-    def delete(self, project_id: UUID4) -> Response:
+    async def delete(self, project_id: UUID4) -> Response:
         """Deleting a Project"""
