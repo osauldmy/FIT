17caa85d263e45552b21d52f33f968fbda6c71fc
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-21 21:27:23 +0200

    Add basic Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../"))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Apixy"
+copyright = "2021, Apixy Team"
+author = "Apixy Team"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+source_suffix = ".rst"
+master_doc = "index"
+highlight_language = "python3"
+pygments_style = "sphinx"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.apidoc",
+    "sphinxcontrib.contentui",
+    "sphinx_autodoc_typehints",
+]
+
+autodoc_default_options = {
+    ":undoc-members:": True,
+    ":private-members:": True,
+    ":show-inheritance:": True,
+    ":inherited-members:": True,
+}
+
+apidoc_module_dir = os.path.join(
+    os.path.dirname(__file__), os.path.pardir, f"{project.lower()}"
+)
+apidoc_output_dir = "."
+apidoc_excluded_paths = ["tests"]
+apidoc_separate_modules = True
+apidoc_module_first = True
+
+typehints_fully_qualified = True
+always_document_param_types = True
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "alabaster"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. Apixy documentation master file, created by
+   sphinx-quickstart on Wed Apr 21 20:26:48 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Apixy's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
