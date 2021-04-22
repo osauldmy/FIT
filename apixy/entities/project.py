3edc25ac731a78ab33b187e653e00719ec14c575
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-22 22:22:09 +0200

    Move optional model fields to the bottom

--- apixy/entities/project.py
+++ apixy/entities/project.py
@@ -7,10 +7,10 @@ from .shared import ForbidExtraModel
 
 class ProjectBase(ForbidExtraModel):
     name: str = Field(max_length=64)
-    description: Optional[str] = Field(default=None, max_length=512)
     slug: str = Field(max_length=64, regex="^[A-Za-z0-9]+(-[A-Za-z0-9]+)*$")
     # the regex matches alphanumeric chars separated with hyphens
     # todo: we might want to do this without a regex
+    description: Optional[str] = Field(default=None, max_length=512)
 
     class Config:
         orm_mode = True
