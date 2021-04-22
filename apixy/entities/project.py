8202b0f394c609d1da5c36e5409e9600b2a051f2
Matyas Richter <mail@mrichter.cz>
2021-04-22 11:40:54 +0200

    Updated the project entity with name and desc.

--- apixy/entities/project.py
+++ apixy/entities/project.py
@@ -1,22 +1,28 @@
 import uuid as uuid_
+from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 from pydantic.types import UUID4
 
+from .shared import ForbidExtraModel
 
-class Project(BaseModel):
-    """The Project entity from our domain model."""
 
-    uuid: UUID4 = Field(default_factory=uuid_.uuid4)
+class ProjectBase(ForbidExtraModel):
+    name: str = Field(max_length=64)
+    description: Optional[str] = Field(default=None, max_length=512)
     slug: str = Field(max_length=64, regex="^[A-Za-z0-9]+(-[A-Za-z0-9]+)*$")
     # the regex matches alphanumeric chars separated with hyphens
     # todo: we might want to do this without a regex
 
 
-class ProjectIn(BaseModel):
-    """Project without uuid, used for creation."""
+class Project(ProjectBase):
+    """The Project entity from our domain model."""
 
-    slug: str
+    uuid: UUID4 = Field(default_factory=uuid_.uuid4)
+
+
+class ProjectIn(ProjectBase):
+    """Project without uuid, used for creation."""
 
     def to_project(self) -> Project:
         """Creates a Project."""
