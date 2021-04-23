fe72a74f4d4ef62e261f97a127d1b4414ea50f9a
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-23 22:37:48 +0200

    Add redis

--- apixy/app.py
+++ apixy/app.py
@@ -1,4 +1,5 @@
 """The app's entrypoint"""
+import aioredis
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
@@ -9,3 +10,14 @@ app = FastAPI(title=SETTINGS.APP_NAME)
 app.mount(SETTINGS.API_PREFIX + "/v1", v1_app)
 
 register_tortoise(app, config=TORTOISE_CONFIG)
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    app.state.redis = await aioredis.create_redis_pool(SETTINGS.REDIS_URI)
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
--- apixy/config.py
+++ apixy/config.py
@@ -17,6 +17,7 @@ class Settings(BaseConfig):
     POSTGRES_PORT: str = environ.get("POSTGRES_PORT", "5432")
     POSTGRES_USER: str = environ.get("POSTGRES_USER", "")
     POSTGRES_PASSWORD: str = environ.get("POSTGRES_PASSWORD", "")
+    REDIS_URI: str = environ.get("REDIS_URI", "redis://localhost")
     DEFAULT_PAGINATION_LIMIT: int = 30
 
     ORIGINS: List[str] = list(
