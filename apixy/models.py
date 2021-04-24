70484e1a936fa034aa7353ced805763f930aaa3b
Matyas Richter <mail@mrichter.cz>
2021-04-24 14:51:37 +0200

    Added a base ORMModel.

--- apixy/models.py
+++ apixy/models.py
@@ -1,13 +1,31 @@
 from __future__ import annotations
 
+from abc import abstractmethod
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel
 from tortoise import fields
 from tortoise.models import Model
 
 from apixy.entities.project import Project as ProjectEntity
 from apixy.entities.project import ProjectBase as ProjectEntityBase
 
+EntityBase = TypeVar("EntityBase", bound=BaseModel)
+Entity = TypeVar("Entity", bound=BaseModel)
 
-class Project(Model):
+
+class ORMModel(Generic[EntityBase, Entity]):
+    @abstractmethod
+    def to_pydantic(self) -> Entity:
+        """Creates a pydantic model from a tortoise model fetched from db."""
+
+    @classmethod
+    @abstractmethod
+    def from_pydantic(cls, entity: EntityBase) -> ORMModel[EntityBase, Entity]:
+        """Creates a tortoise model instance from a pydantic model."""
+
+
+class Project(ORMModel[ProjectEntityBase, ProjectEntity], Model):
     """The DB entity for Project"""
 
     id = fields.IntField(pk=True)
