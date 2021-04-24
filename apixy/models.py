0c98822a6a0fdddca9fbc85301f76eda4e3e0c6b
Matyas Richter <mail@mrichter.cz>
2021-04-24 10:15:53 +0200

    Made Project.slug unique.

--- apixy/models.py
+++ apixy/models.py
@@ -5,6 +5,7 @@ from tortoise.models import Model
 class Project(Model):
     """The DB entity for Project"""
 
-    slug = fields.CharField(128)
+    # slug is globally unique for now - this might need to be changed in the future
+    slug = fields.CharField(128, unique=True)
     name = fields.CharField(64)
     description = fields.CharField(512, null=True)
