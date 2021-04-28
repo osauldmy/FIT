ac88f180557d554423dd3a291f2c81128467a1c2
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-28 22:17:43 +0200

    Fix migration (manually) with sequence renaming

--- apixy/migrations/models/7_20210428163325_update.sql
+++ apixy/migrations/models/7_20210428163325_update.sql
@@ -1,6 +1,8 @@
 -- upgrade --
 ALTER TABLE "DataSource" RENAME TO "datasource";
+ALTER SEQUENCE "DataSource_id_seq" RENAME TO "datasource_id_seq";
 ALTER TABLE "Project" RENAME TO "project";
 -- downgrade --
 ALTER TABLE "project" RENAME TO "Project";
 ALTER TABLE "datasource" RENAME TO "DataSource";
+ALTER SEQUENCE "datasource_id_seq" RENAME TO "DataSource_id_seq";
