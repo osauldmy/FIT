64baa070b0cd0c754ad7c602b3c0aadeb1d1b70e
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-30 01:01:31 +0200

    Add fetch_data() for project

--- apixy/entities/project.py
+++ apixy/entities/project.py
@@ -1,9 +1,13 @@
-from typing import List, Optional
+import asyncio
+import logging
+import traceback
+from typing import List, Optional, Union
 
 from pydantic import Field
 
-from .datasource import DataSourceUnion
+from .datasource import HTTPDataSource, MongoDBDataSource, SQLDataSource
 from .merge_strategy import MERGE_STRATEGY_MAPPING
+from .proxy_response import ProxyResponse
 from .shared import ForbidExtraModel, OmitFieldsConfig
 
 
@@ -21,7 +25,42 @@ class Project(ForbidExtraModel):
 
 
 class ProjectWithDataSources(Project):
-    datasources: List[DataSourceUnion]
+    datasources: List[Union[HTTPDataSource, MongoDBDataSource, SQLDataSource]]
+
+    async def fetch_data(
+        self, include_exceptions: bool = True, include_tracebacks: bool = False
+    ) -> ProxyResponse:
+
+        fetched, errors = [], []
+
+        for datasource in self.datasources:
+            try:
+                fetched.append(await datasource.fetch_data())
+            except Exception as exception:  # pylint: disable=broad-except
+                logging.getLogger(__name__).exception(exception)
+
+                if include_tracebacks:
+                    error_message = traceback.format_exc()
+                elif include_exceptions:
+                    error_message = str(exception)
+                else:
+                    error_message = "Error happened!"
+
+                # not the best fix, but at least it works
+                if error_message == "" and isinstance(exception, asyncio.TimeoutError):
+                    error_message = "Timeout!"
+
+                # TODO: `datasource.url` -> `datasource.name` after caching MR merge.
+                errors.append({datasource.url: error_message})
+
+        merged = MERGE_STRATEGY_MAPPING[self.merge_strategy].apply(fetched)
+        return ProxyResponse(
+            result={
+                "size": len(fetched),
+                "data": merged,
+            },
+            errors={"size": len(errors), "data": errors},
+        )
 
 
 class ProjectInput(Project):
