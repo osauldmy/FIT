3bdcd8eac5690c372b35cdb1093bb2e887b119b0
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-04-30 01:02:04 +0200

    Update fetch view with new routine

--- apixy/api/v1/fetch.py
+++ apixy/api/v1/fetch.py
@@ -1,29 +1,27 @@
-from typing import Final, List, Optional
+from typing import Final, Optional
 
 from fastapi import APIRouter, HTTPException, Query
 from starlette import status
 from tortoise.exceptions import DoesNotExist
 
 from apixy import models
-from apixy.api.v1.datasources import DataSourceUnion
+from apixy.entities.proxy_response import ProxyResponse
 
 PREFIX_USER: Final[str] = "/collect"  # TODO: discuss possible prefixes
 
 router = APIRouter(tags=["FetchRouter"])
 
 
-# TODO: add appropriate data response model
-@router.get(PREFIX_USER + "/{project_slug}", response_model=List[DataSourceUnion])
+@router.get(PREFIX_USER + "/{project_slug}", response_model=ProxyResponse)
 async def fetch(
     project_slug: Optional[str] = Query(
         None, max_length=64, regex="^[A-Za-z0-9]+(-[A-Za-z0-9]+)*$"
-    ),
-) -> List[DataSourceUnion]:
+    )
+) -> ProxyResponse:
     """Fetches and aggregates all data sources tied to project slug."""
     try:
         model = await models.Project.get(slug=project_slug)
         project = await model.to_pydantic_with_datasources()
-        # TODO: add fetching functionality
-        return project.datasources
+        return await project.fetch_data()
     except DoesNotExist as err:
         raise HTTPException(status.HTTP_404_NOT_FOUND) from err
