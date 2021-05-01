d0ec4bf216562fe72582b6476669415fdd0ea548
Jakub Kučera <kucerj56@fit.cvut.cz>
2021-05-01 18:38:29 +0200

    Added mock test for project.fetch_data

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -1,9 +1,12 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
+from unittest import mock
 
 import pytest
 from pydantic.error_wrappers import ValidationError
 
-from apixy.entities.project import Project
+from apixy.entities.datasource import HTTPDataSource, MongoDBDataSource, SQLDataSource
+from apixy.entities.project import Project, ProjectWithDataSources
+from tests.unit.datasource_json_responses.spacex_rockets import PAYLOAD_SPACEX_ROCKETS
 
 
 @pytest.fixture
@@ -46,3 +49,114 @@ def test_invalid_merge_strategy(sample_project_data: Dict[str, Any]) -> None:
     with pytest.raises(ValidationError):
         sample_project_data.update(merge_strategy="conCATeNATION")
         Project(**sample_project_data)
+
+
+datasource_name_to_class = {
+    "HTTPDataSource": HTTPDataSource,
+    "MongoDBDataSource": MongoDBDataSource,
+    "SQLDataSource": SQLDataSource,
+}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "data_sources_with_payloads",
+    [
+        (
+            (
+                "HTTPDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+        ),
+        (
+            (
+                "MongoDBDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+        ),
+        (
+            (
+                "SQLDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+        ),
+        (
+            (
+                "HTTPDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+            (
+                "HTTPDataSource",
+                ["Falcon 1", "Falcon 9", "Falcon Heavy", "Starship"],
+            ),
+        ),
+        (
+            (
+                "HTTPDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+            (
+                "HTTPDataSource",
+                [{"foo2": "bar2"}, {"bar2": "baz2"}],
+            ),
+            (
+                "SQLDataSource",
+                [
+                    ["Falcon 1", 40],
+                    ["Falcon 9", 98],
+                    ["Falcon Heavy", 100],
+                    ["Starship", 0],
+                ],
+            ),
+        ),
+        (
+            (
+                "HTTPDataSource",
+                [{"foo": "bar"}, {"bar": "baz"}],
+            ),
+            (
+                "HTTPDataSource",
+                PAYLOAD_SPACEX_ROCKETS,
+            ),
+            (
+                "SQLDataSource",
+                [{"foo3": "bar3"}, {"bar3": "baz3"}],
+            ),
+        ),
+    ],
+)
+async def test_project_fetch_data_concatenation(
+    data_sources_with_payloads: Tuple[Tuple[str, List[Any]], ...]
+) -> None:
+    data_sources = []
+    fetched_payloads = []
+
+    for data_source_class_name, payload in data_sources_with_payloads:
+        data_source_class = datasource_name_to_class[data_source_class_name]
+
+        with mock.patch(
+            "apixy.entities.datasource." + data_source_class_name,
+            spec=data_source_class,
+            spec_set=True,
+        ) as mock_data_source:
+            data_source = mock_data_source.return_value
+
+        data_source.copy.return_value.fetch_data = mock.AsyncMock(return_value=payload)
+
+        fetched_payloads.append(payload)
+        data_sources.append(data_source)
+
+    project = ProjectWithDataSources(
+        name="TestingDemoName",
+        slug="testing-demo-name",
+        merge_strategy="concatenation",
+        datasources=data_sources,
+    )
+
+    fetched_project_data = await project.fetch_data()
+
+    for data_source in data_sources:
+        data_source.copy.return_value.fetch_data.assert_awaited_once()
+
+    desired_data = {str(i): p for i, p in enumerate(fetched_payloads)}
+    assert desired_data == fetched_project_data.result.data
