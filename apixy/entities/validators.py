80ff353ce589142afe91048ea69809a8f3e33318
Matyas Richter <mail@mrichter.cz>
2021-05-02 15:21:08 +0200

    Added a docstring.

--- apixy/entities/validators.py
+++ apixy/entities/validators.py
@@ -2,6 +2,10 @@ from typing import Optional, Sized
 
 
 def validate_nonzero_length(value: Optional[Sized]) -> Optional[Sized]:
+    """
+    Value can be either None or not empty.
+    Disallows things like `[]`, `{}` etc.
+    """
     if value is not None and len(value) == 0:
         raise ValueError("Must be either null or not empty.")
     return value
