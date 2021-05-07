0d86ec07ae4db82adcdf1375f0fe4ac9ebafc2c6
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-05-07 18:28:34 +0200

    Add basic tests for recursive merge strategy

--- tests/unit/test_merge_strategy.py
+++ tests/unit/test_merge_strategy.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, List
+from collections.abc import Collection
+from typing import Any, Dict, Iterable
 
 import pydantic
 import pytest
@@ -6,6 +7,7 @@ import pytest
 from apixy.entities.merge_strategy import (
     MERGE_STRATEGY_MAPPING,
     ConcatenationMergeStrategy,
+    RecursiveMergeStrategy,
 )
 
 
@@ -48,9 +50,138 @@ class TestConcatenationMergeStrategy:
                 [{"a": "b"}, {"b": "c"}, {"c": "d"}],
                 {"0": {"a": "b"}, "1": {"b": "c"}, "2": {"c": "d"}},
             ),
+            (
+                [[1, 2, 3], None],
+                {"0": [1, 2, 3], "1": None},
+            ),
         ),
     )
-    def test_concatenation_merge_strategy(
-        inputs: List[Any], outputs_reference: Dict[str, Any]
+    def test_concatenation_merge_strategy_success(
+        inputs: Iterable[Any], outputs_reference: Dict[str, Any]
     ) -> None:
         assert ConcatenationMergeStrategy().apply(inputs) == outputs_reference
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "inputs,outputs_reference",
+        (),
+    )
+    def test_concatenation_merge_strategy_failure(
+        inputs: Iterable[Any], outputs_reference: Dict[str, Any]
+    ) -> None:
+        assert ConcatenationMergeStrategy().apply(inputs) == outputs_reference
+
+
+class TestRecursiveMergeStrategy:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "inputs,outputs",
+        (
+            # Handling single datasource output
+            ((None,), None),
+            (([],), []),
+            (("foo",), "foo"),
+            ((["foo", "bar"],), ["foo", "bar"]),
+            # Ignoring Nones
+            (([0, 1, 2, 3], None), [0, 1, 2, 3]),
+            (([0, 1, 2, 3], None, None), [0, 1, 2, 3]),
+            ((None, [0, 1, 2, 3], None), [0, 1, 2, 3]),
+            # Reducing duplicates
+            ((None, None), None),
+            ((["a", "b"], ["a", "b"]), ["a", "b"]),
+            (([0, 1], [0, 1]), [0, 1]),
+            (
+                (
+                    {"a": [1, 2, 3], "b": [4, 5, 6]},
+                    {"a": [1, 2, 3], "b": [4, 5, 6]},
+                ),
+                {"a": [1, 2, 3], "b": [4, 5, 6]},
+            ),
+            # Concatenating lists
+            (
+                ([0, 1, 2, 3], [4, 5, 6]),
+                [0, 1, 2, 3, 4, 5, 6],  # may be different order
+            ),
+            (
+                (["a", "b", "c"], ["d", "e", "f"]),
+                ["a", "b", "c", "d", "e", "f"],  # may be different order
+            ),
+            (("abc", "def"), ["abc", "def"]),
+            (("abc", "def", "ghi"), ["abc", "def", "ghi"]),
+            # Edge-cases
+            (
+                (
+                    {"a": [1, 2, 3], "b": [4, 5, 6, 7]},
+                    {"a": [1, 2, 3], "b": [4, 5, 6]},
+                ),
+                {"a": [1, 2, 3], "b": [4, 5, 6, 7]},
+            ),
+            (
+                (
+                    {"a": [1, 2, 3], "b": [4, 5, 6]},
+                    {"a": [1, 2, 3], "b": [4, 5, 6, 7]},
+                ),
+                {"a": [1, 2, 3], "b": [4, 5, 6, 7]},
+            ),
+            (
+                (
+                    {"a": [1, 2, 3, 4], "b": [4, 5, 6]},
+                    {"a": [1, 2, 3], "b": [4, 5, 6, 7]},
+                ),
+                {"a": [1, 2, 3, 4], "b": [4, 5, 6, 7]},
+            ),
+            (
+                (
+                    {"a": [1, 2, 3, 4]},
+                    {"b": [4, 5, 6, 7]},
+                ),
+                {"a": [1, 2, 3, 4], "b": [4, 5, 6, 7]},
+            ),
+            (
+                (
+                    {"a": [1, 2, 3, 4], "b": [4, 5, 6]},
+                    {"c": [1, 2, 3], "d": [4, 5, 6, 7]},
+                ),
+                {
+                    "a": [1, 2, 3, 4],
+                    "b": [4, 5, 6],
+                    "c": [1, 2, 3],
+                    "d": [4, 5, 6, 7],
+                },
+            ),
+            (
+                (
+                    {"a": [1, 2, 3, 4], "b": [4, 5, 6]},
+                    {"b": [1, 2, 3], "c": [4, 5, 6, 7]},
+                ),
+                {
+                    "a": [1, 2, 3, 4],
+                    "b": [1, 2, 3, 4, 5, 6],
+                    "c": [4, 5, 6, 7],
+                },
+            ),
+            (
+                (
+                    {"a": [1, 2, 3, 4], "b": [4, 5, 6]},
+                    {"b": [4, 5, 6], "c": [4, 5, 6, 7]},
+                ),
+                {
+                    "a": [1, 2, 3, 4],
+                    "b": [4, 5, 6],
+                    "c": [4, 5, 6, 7],
+                },
+            ),
+            (
+                ({"a": "b"}, "foobar"),
+                [{"a": "b"}, "foobar"],
+            ),
+            (
+                ({"a": "b"}, [1, 2, 3]),
+                [{"a": "b"}, [1, 2, 3]],
+            ),
+        ),
+    )
+    def test_simple_merge_success(
+        inputs: Iterable[Any], outputs: Collection[Any]
+    ) -> None:
+        assert RecursiveMergeStrategy().apply(inputs) == outputs
