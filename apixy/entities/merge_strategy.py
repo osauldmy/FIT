2935995decc6fa531f2b792ea35b44b47f0911de
Dmytro Osaulenko <osauldmy@fit.cvut.cz>
2021-05-08 11:48:34 +0200

    Refactor recursive merge strategy apply._reduce()

--- apixy/entities/merge_strategy.py
+++ apixy/entities/merge_strategy.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
 from collections.abc import Collection
 from functools import reduce
-from typing import Any, Final, Iterable, Mapping
+from typing import Any, Dict, Final, Iterable, List, Mapping, Union
 
 from apixy.entities.shared import ForbidExtraModel
 
@@ -39,6 +39,37 @@ class RecursiveMergeStrategy(MergeStrategy):
 
     @staticmethod
     def apply(data: Iterable[Any]) -> Collection[Any]:
+        def _reduce_dicts(
+            left: Dict[Any, Any], right: Dict[Any, Any]
+        ) -> Dict[Any, Any]:
+            if left.keys() == right.keys():
+                for key in left:
+                    right[key] = _reduce(left[key], right[key])
+                return right
+
+            if intersection := left.keys() & right.keys():
+                new_dict = {}
+                for key in left.keys() | right.keys():
+                    if key in intersection:
+                        if left[key] == right[key]:
+                            new_dict[key] = left[key]
+                        else:
+                            new_dict[key] = _reduce(left[key], right[key])
+                    else:
+                        new_dict[key] = left.get(key) or right.get(key)
+
+                return new_dict
+
+            left.update(right)
+            return left
+
+        def _reduce_list_with_scalar(
+            left: List[Any], right: Union[str, int, float, bool]
+        ) -> List[Any]:
+            if right not in left:
+                left.append(right)
+            return left
+
         def _reduce(left: Any, right: Any) -> Any:
 
             if left == right:
@@ -47,52 +78,26 @@ class RecursiveMergeStrategy(MergeStrategy):
             if left is None or right is None:
                 return left or right
 
-            if type(left) is type(right):
-                if isinstance(left, str):
-                    return [left, right]
+            return_value: Any = [left, right]
 
+            if type(left) is type(right):
                 if isinstance(left, list):
-                    return sorted(
+                    return_value = sorted(
                         set(left)
                         .intersection(right)
                         .union(set(left).symmetric_difference(right))
                     )
 
                 if isinstance(left, dict):
-                    if left.keys() == right.keys():
-                        for key in left:
-                            ret = _reduce(left[key], right[key])
-                            right[key] = ret
-                        return right
-
-                    if intersection := left.keys() & right.keys():
-                        new_dict = {}
-                        for key in left.keys() | right.keys():
-                            if key in intersection:
-                                if left[key] == right[key]:
-                                    new_dict[key] = left[key]
-                                else:
-                                    new_dict[key] = _reduce(left[key], right[key])
-                            else:
-                                new_dict[key] = left.get(key) or right.get(key)
-                        return new_dict
-
-                    left.update(right)
-                    return left
+                    return_value = _reduce_dicts(left, right)
 
             if isinstance(left, list) and isinstance(right, (str, int, float, bool)):
-                if right not in left:
-                    left.append(right)
-                    return left
-                return left
+                return_value = _reduce_list_with_scalar(left, right)
 
             if isinstance(right, list) and isinstance(left, (str, int, float, bool)):
-                if left not in right:
-                    right.append(left)
-                    return right
-                return right
+                return_value = _reduce_list_with_scalar(left=right, right=left)
 
-            return [left, right]
+            return return_value
 
         return reduce(_reduce, data)
 
