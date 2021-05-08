46213bc480d445c8b63d6b77c9cb824ffb86d6fc
Matyas Richter <mail@mrichter.cz>
2021-05-08 22:12:15 +0200

    Added the FetchLogger interface.

--- apixy/entities/project.py
+++ apixy/entities/project.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
-from typing import Any, Dict, List, Optional, Type, Union
+import time
+from functools import wraps
+from typing import Any, Awaitable, Callable, List, Optional, Tuple, Union, cast, Union, List, Optional
 
 from pydantic import BaseModel, Field
 
@@ -15,6 +17,24 @@ from .proxy_response import ProxyResponse
 from .shared import ForbidExtraModel, OmitFieldsConfig
 
 logger = logging.getLogger(__name__)
+
+
+class FetchLogger:
+    async def save_log(
+        self, datasource_id: int, nanoseconds: int, success: bool
+    ) -> None:
+        raise NotImplementedError()
+
+    @staticmethod
+    async def fetch_timer(coroutine: Callable[[], Awaitable[Any]]) -> Any:
+        @wraps(coroutine)
+        async def wrapped() -> Tuple[Any, int]:
+            time_start = time.perf_counter_ns()
+            result = await coroutine()
+            time_measured = time.perf_counter_ns() - time_start
+            return result, time_measured
+
+        return wrapped
 
 
 class Project(ForbidExtraModel):
@@ -40,26 +60,38 @@ class Project(ForbidExtraModel):
 class ProjectWithDataSources(Project):
     datasources: List[Union[HTTPDataSource, MongoDBDataSource, SQLDataSource]]
 
-    async def fetch_data(self) -> ProxyResponse:
+    async def fetch_data(self, fetch_logger: FetchLogger) -> ProxyResponse:
 
         fetched, errors = [], []
 
         gathered = await asyncio.gather(
-            *(datasource.fetch_data() for datasource in self.datasources),
+            *(
+                fetch_logger.fetch_timer(datasource.fetch_data)
+                for datasource in self.datasources
+            ),
             return_exceptions=True
         )
 
-        for index, result in enumerate(gathered):
+        for index, (result, nanoseconds) in enumerate(gathered):
             if not isinstance(result, Exception):
                 fetched.append(result)
+                success = True
             else:
                 logger.exception(result)
-
+                success = False
                 # TODO: `datasource.url` -> `datasource.name` after caching MR merge.
                 if isinstance(result, asyncio.TimeoutError):
                     errors.append({self.datasources[index].url: "Timeout!"})
                 elif isinstance(result, DataSourceFetchError):
                     errors.append({self.datasources[index].url: "Fetch error!"})
+
+            if self.datasources[index].id is None:
+                raise ValueError("DataSource has no ID.")
+            asyncio.create_task(
+                fetch_logger.save_log(
+                    cast(int, self.datasources[index].id), nanoseconds, success=success
+                )
+            )
 
         merged = MERGE_STRATEGY_MAPPING[self.merge_strategy].apply(fetched)
         return ProxyResponse(
